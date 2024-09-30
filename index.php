418ddb65d5c6e767ac70da5eb7ec3277980b1532
H4n20 <165984049+H4n20@users.noreply.github.com>
2024-09-30 17:08:37 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản lý thiết bị</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+    <!-- Header -->
+    <div id="header" style='text-align:center'>
+        <?php include('template/header1.php'); ?>
+    </div>
+
+    <!-- Main Content Area -->
+    <div id="main">
+        <!-- Main Content -->
+        <div id="content">
+            <?php include('template/center.php'); ?>
+        </div>
+    </div>
+    <div id="footer">
+        <?php include('template/footer.php'); ?>
+    </div>
+
+</body>
+</html>
+
+
